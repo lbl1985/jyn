b1b7a14e62019075d1a51f866eb6e17905ac1164
Binlong Li <bil@microsoft.com>
2017-03-18 11:22:51 -0700

    initialize the jyn system website.

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var pug = require('pug');
+
+var routes = require('./routes/web_routes');
+
+var app = express();
+
+app.locals.appName = "Qingdao Jieyina Package Management System";
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "jade");
+app.set("views", path.resolve(__dirname, "views"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+    secret: require('crypto').randomBytes(64).toString(hex),
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(routes);
+
+app.listen(app.get("port"), function(){
+    console.log("Server started on port " + app.get("port"));
+})
+
+
